e52edf4b4786c75b747b56c5f72f2364feaa3bcd
Kritii <kritikumari53782@gmail.com>
2026-03-27 22:51:05 +0530

    added

--- p3.js
+++ p3.js
@@ -1,1 +1,4 @@
 const accessKey="HL0Hhz9ucoty-V4JQ32jB6RlCFL4vaQ3T-W8W7X3Imc";
+const searchForm=document.getElementById("search-form");
+const searchBox=document.getElementById("search-box");
+const searchResult=document.getElementById("search-result");
